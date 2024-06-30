5620f461a6c1a1e62aa3c3e3dcd5e66c4ab86a0d
Jacob Piirsalu <jcpiirsa@uwaterloo.ca>
2024-06-30 17:39:22 -0400

    commiting keyboard pressing

--- main.go
+++ main.go
@@ -1,7 +1,70 @@
 package main
 
-import "fmt"
+import (
+    "strconv"
+    "sync"
+    "time"
+	"strings"
+
+    "fyne.io/fyne/v2/app"
+    "fyne.io/fyne/v2/container"
+    "fyne.io/fyne/v2/widget"
+    "github.com/go-vgo/robotgo"
+)
+
 
 func main() {
-    fmt.Println("Hello, World!")
+    myApp := app.New()
+    myWindow := myApp.NewWindow("AutoKeyPress")
+
+    intervalEntry := widget.NewEntry()
+    intervalEntry.SetPlaceHolder("Interval in milliseconds")
+
+    var stopChan chan struct{}
+    var wg sync.WaitGroup
+
+    startButton := widget.NewButton("Start", func() {
+        intervalStr := intervalEntry.Text
+        interval, err := strconv.Atoi(strings.TrimSpace(intervalStr))
+        if err != nil {
+            return
+        }
+
+        stopChan = make(chan struct{})
+        wg.Add(1)
+
+        go func() {
+            defer wg.Done()
+            ticker := time.NewTicker(time.Duration(interval) * time.Millisecond)
+            defer ticker.Stop()
+
+            for {
+                select {
+                case <-ticker.C:
+                    robotgo.KeyTap("space")
+                case <-stopChan:
+                    return
+                }
+            }
+        }()
+    })
+
+    stopButton := widget.NewButton("Stop", func() {
+        if stopChan != nil {
+            close(stopChan)
+            wg.Wait()
+            stopChan = nil
+        }
+    })
+
+    content := container.NewVBox(
+        widget.NewLabel("Enter the interval between spacebar presses:"),
+        intervalEntry,
+        startButton,
+        stopButton,
+    )
+
+    myWindow.SetContent(content)
+    myWindow.ShowAndRun()
 }
+ 
